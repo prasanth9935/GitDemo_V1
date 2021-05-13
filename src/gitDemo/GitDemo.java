397fcaaec30dfa98a2f22bca75c06a3f892fb970
Naga Prasanth <Naga Prasanth@DESKTOP-PEASIPU>
2021-05-13 11:43:00 +0530

    First Commit

---
+++ src/gitDemo/GitDemo.java
@@ -0,0 +1,13 @@
+package gitDemo;
+
+public class GitDemo {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String name="B N Prasanth";
+		System.out.println(name);
+
+	}
+
+}
